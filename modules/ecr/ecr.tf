cdd4e78ea5e586eadfebae94c9a548672417c3f4
Yaru Anisovec <anisovets@gmail.com>
2023-11-01 17:44:01 -0400

    region added

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+    region = var.region
+}
+
 resource "aws_ecr_repository" "this" {
   name                 = var.repository_name
   image_tag_mutability = var.image_tag_mutability
